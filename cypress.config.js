768285273def3c4c18ae57bab1823e316a2cac04
Leo Costa <leolpc21@hotmail.com>
2024-07-08 08:49:47 -0300

    Configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -4,14 +4,15 @@ module.exports = defineConfig({
   projectId: 'taycfc',
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
-    defaultCommandTimeout : 10000,
+    defaultCommandTimeout: 10000,
     requestTimeout: 10000,
     chromeWebSecurity: false,
     env: {
       viewportWidthBreakpoint: 768,
     },
-    setupNodeEvents() {
-      // implement node event listeners here
+    setupNodeEvents(config) {
+      require('@cypress/grep/src/plugin')(config)
+      return config
     },
   },
 })
